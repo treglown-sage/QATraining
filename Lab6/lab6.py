afd17481c1bdf93a348eeeaf527d08ebca758e3d
treglown-sage <carl.treglown@sage.com>
2023-05-03 11:25:21 +0100

    Fixed tax Calculator

--- Lab6/lab6.py
+++ Lab6/lab6.py
@@ -5,16 +5,16 @@ def tax_calculator(pay, percentage):
 
 def getIncomeTax(income: float)->float:
     if income <= personal_allowance:
-        return 0 
+        return tax_calculator (income, 0)
     elif  income > personal_allowance and income <34500:
-        return tax_calculator (income, 20) 
+        return tax_calculator (income - personal_allowance, 20) 
     elif income > 34500 and income < 150000:
-        return tax_calculator(income, 40) 
+        return tax_calculator(income - personal_allowance, 40) 
     elif income > 150000:
-        return tax_calculator(income, 45) 
+        return tax_calculator(income - personal_allowance, 45) 
     else:
         print("You must input your annual income!")
 
-income = float(input("Input your annual income: £")) - personal_allowance
+income = float(input("Input your annual income: £"))
 calculated_tax = getIncomeTax(income) 
-print(f"Your annual tax is £{calculated_tax}")  
+print(f"Your annual tax is £{calculated_tax} with income of {income}")  
